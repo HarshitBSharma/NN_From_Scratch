f5975447a96f04f05f574e4229aeb4dc11dc372b
Harshit Sharma <harshit-sharma@riseup.net>
2020-07-31 11:24:11 +0530

    Finished backprop

--- NN_complete.py
+++ NN_complete.py
@@ -1,129 +1,155 @@
 import numpy as np
 np.random.seed(0)
+
 class NeuralNetwork:
     def __init__(self):
         self.layer = []
         self.parameters = []
-        self.forward_cache = {}
-
-    def add(self, layer_size, activation_function='relu'):
-        self.layer.append({"neurons": layer_size, "activation": activation_function})
-
+        self.caches = []
+        self.gradients = {}
+        
+    def add(self, layer_size, activation_function="relu"):
+        self.layer.append({"neurons": layer_size, "activation":activation_function})
 
     def initialize_parameters(self):
         L = len(self.layer)
-        self.parameters.append([])
         for i in range(1, L):
-            #w_temp =  0.01 * np.random.randn(self.layer[i]["neurons"], self.layer[i-1]["neurons"])
-            #b_temp =  0.01 * np.random.randn(self.layer[i]["neurons"], 1)
-            w_temp = 2 * np.ones((self.layer[i]["neurons"], self.layer[i-1]["neurons"]))
-            b_temp = 2 * np.ones((self.layer[i]["neurons"], 1))
-            self.parameters.append({"W": w_temp, "b":b_temp})
-        
+            W_temp = np.random.randn(self.layer[i]["neurons"], self.layer[i-1]["neurons"]) * 0.1
+            b_temp = np.random.randn(self.layer[i]["neurons"], 1) * 0.1
+            # W_temp = 2 * np.ones((self.layer[i]["neurons"], self.layer[i-1]["neurons"]))
+            # b_temp = 2 * np.ones((self.layer[i]["neurons"], 1))
+            self.parameters.append({"W": W_temp, "b":b_temp})
+
+            """self.parameters["W" + str(i)] = W_temp
+            self.parameters["b" + str(i)] = b_temp"""
+
+
     def forward_step(self, W, A_prev, b, activation_function):
-        Z_temp = np.dot(W, A_prev) + b
-        A_temp = self.activation(Z_temp, activation_function)
-        return Z_temp, A_temp
+        # print(f"W: {W}, A_prev : {A_prev}, b: {b}")
+        Z = np.dot(W, A_prev) + b
+        A = self.activation(Z, activation_function)
+        return {"W": W, "b": b, "A_prev": A_prev, "Z": Z, "A": A}
 
 
     def activation(self, Z, activation_function):
         if activation_function == "relu":
             A = np.array(np.maximum(0, Z))
-        return A
+        elif activation_function == "sigmoid":
+            A = 1/(1+np.exp(-Z))    
+        return A    
+
 
     def forward_prop(self, inputs):
-        self.inputs = inputs
+        self.caches = []
         A_prev = inputs
-        L = len(self.layer)
+        L = len(self.parameters)
         for i in range(1, L):
-            W = self.parameters[i]["W"]
-            b = self.parameters[i]["b"]
-            activation_function = self.layer[i]["activation"]
-            Z, A = self.forward_step(W, A_prev, b, activation_function)
-            self.forward_cache["Z" + str(i)] = Z
-            self.forward_cache["A" + str(i)] = A
-            A_prev = A
-            #print(f"W is {W} and b is {b}")
+            """print(f"Currently on layer {i} and values are " + 
+                    f"W: {self.parameters[i-1]['W']} " +
+                    f"A_prev: {A_prev} " +
+                    f"b: {self.parameters[i-1]['b']} " +
+                    f"Activation function: {self.layer[i]['activation']}")"""
+            cache = self.forward_step(self.parameters[i-1]["W"], A_prev, self.parameters[i-1]["b"], self.layer[i]["activation"])
+            A_prev = cache["A"]
+            #print("Calculated cache is ")
+            self.caches.append(cache)
+        cache = self.forward_step(self.parameters[-1]["W"], A_prev, self.parameters[-1]["b"], self.layer[-1]["activation"])
+        self.caches.append(cache)
+        return self.caches
 
     
-    def back_prop(self, outputs):
-        m=3
-        L = len(self.layer)
-        #Getting parameters
-        W2 = self.parameters[2]["W"]
-
-        #Getting Forward cache
-
-        prediction = self.forward_cache["A" + str(L-1)]
-        print(f"Prediction:{prediction}")
-        Z_last = self.forward_cache["Z" + str(L-1)]
-        #print(f"Z_last: {Z_last}")
-
-        dA2 = -2 * (outputs-prediction)
-        dZ2 = np.array(dA2, copy=True)
-        dZ2[Z_last <= 0] = 0
-        print(f"dA2: {dA2}, dZ2 : {dZ2}")
-        dW2 = (1/m) * np.dot(dZ2, self.forward_cache["A" + str(1)].T)
-        db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True)
-        learning_rate = 0.01
-
-        Z1 = self.forward_cache["Z" + str(1)]
-        A1 = self.forward_cache["A" + str(1)]
-        print(f"dW2: {dW2} db2: {db2}")
-        dA1 = np.dot(W2.T, dZ2)
-        dZ1 = np.array(dA1, copy=True)
-        dZ1[Z1 <= 0] = 0
-        print(f"dA1 = {dA1}, dZ1 = {dZ1}, Z1 = {Z1}")
-        print(f"Before calculating dw1, dZ1 : {dZ1} inputs.T : {self.inputs.T}")
-        dW1 = (1/m) * np.dot(dZ1, self.inputs.T)
-        db1 = (1/m) * np.sum(dZ1, axis=1, keepdims = True)
-
-        print(f"dW1: {dW1}, db1 = {db1}")
-
-        #Updating parameters
-        self.parameters[2]["W"] -= learning_rate * dW2
-        self.parameters[2]["b"] -= learning_rate * db2
-
-        self.parameters[1]["W"] -= learning_rate * dW1
-        self.parameters[1]["b"] -= learning_rate * db1
+    def calculate_cost(self, AL, Y):
+        m = AL.shape[1]
+        cost = (1/m)*np.sum((Y - AL) ** 2)
+        cost = np.squeeze(cost)
+        return cost
 
 
-    def update_parameters(self):
-        for i in range(1, len(self.layer)-1):
-            self.parameters[i]["W"] -= learning_rate * dW2
-            self.parameters[i]["b"] -= learning_rate * db2
+    def backward_step(self, dZ, cache):
+        A_prev = cache["A_prev"]
+        W = cache["W"]
+        b = cache["b"]
+        m = A_prev.shape[1]
+        dW = (1/m) * np.dot(dZ, A_prev.T)
+        db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(W.T, dZ)
 
-"""        W = self.parameters[2]["W"]
-        b = self.parameters[2]["b"]
-        W -= learning_rate * dW
-        b -= learning_rate * db
-        self.parameters[1]["W"] = W
-        self.parameters[1]["b"] = b """
-
+        return dW, dA_prev, db
 
 
+    def get_g_prime(self, dA, cache, activation_function):
+        if activation_function == "relu":
+            dZ = np.array(dA, copy=True)
+            Z = cache["Z"]
+            # print(f"Z: {Z}, dZ:{dZ}")
+            dZ[Z <= 0] = 0
+        elif activation_function == "sigmoid":
+            t = 1/(1 + exp(-cache["Z"]))
+            dZ = dA * t * (1-t)
+        return dZ
 
+
+    def back_prop(self, Y):
+        self.gradients = {}
+        AL = self.caches[-1]["A"]
+        L = len(self.caches)
+        Y = Y.reshape(AL.shape)
+
+        dAL = -2 * (Y-AL)
+        current_cache = self.caches[-1]
+        dZ = self.get_g_prime(dAL, current_cache, self.layer[-1]["activation"])
+        dW, dA_prev, db = self.backward_step(dZ, current_cache)
+        self.gradients["dW" + str(L)] = dW
+        self.gradients["dA_prev" + str(L-1)] = dA_prev
+        self.gradients["db" + str(L)] = db
+        
+        for l in reversed(range(L-1)):
+            # print(f"l is {l} and L is {L}")
+            current_cache = self.caches[l]
+            dZ = self.get_g_prime(self.gradients["dA_prev" + str(l+1)], current_cache, self.layer[l]["activation"])
+            dW, dA_prev, db = self.backward_step(dZ, current_cache)
+            self.gradients["dW" + str(l+1)] = dW
+            self.gradients["dA_prev" + str(l)] = dA_prev
+            self.gradients["db" + str(l+1)] = db
+        
+        return self.gradients
+
+    def train(self, inputs, outputs, epochs):
+        self.initialize_parameters()
+        for i in range(epochs):
+            self.forward_prop(inputs)
+            self.back_prop(outputs)
+
+    def update_parameters(self, learning_rate=0.01):
+        L = len(self.parameters)
+        for l in range(L):
+            # print(f"Inside the for loop, l={l}, W: {self.parameters[l]['W']}, dW: {self.gradients['dW' + str(l+1)]}")
+            self.parameters[l]["W"] = self.parameters[l]["W"] - learning_rate*self.gradients["dW" + str(l+1)]
+            self.parameters[l]["b"] = self.parameters[l]["b"] - learning_rate*self.gradients["db" + str(l+1)]
 
 
     
 
+    
 inputs = np.array([[1, 2, 3], [4, 5, 6]])
-print(inputs.shape)
+#print(inputs.shape)
 outputs = np.array([5, 7, 9]).reshape(1, 3)
-print(outputs.shape)
+#print(outputs.shape)
 model = NeuralNetwork()
 model.add(2)
 model.add(3)
 model.add(1)
-#print(model.layer)
 model.initialize_parameters()
-print("Before starting backprop")
-print(model.parameters)
-print("Starting forwardprop")
-# model.forward_prop(inputs)
-# print(model.forward_cache)
-for i in range(2):
+
+for i in range(500):
     model.forward_prop(inputs)
+    #print(model.caches)
     model.back_prop(outputs)
+    #print(model.caches)
+    #print(model.gradients)
+    model.update_parameters()
     #print(model.forward_cache)
-print(model.parameters)
+    print(model.caches[-1]["A"])
+
+
+        
